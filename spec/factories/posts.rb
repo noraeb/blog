ec19ecd18e3baad45684448ab6f3c0dd7689a724
noraeb <n.blomaard@gmail.com>
2016-05-10 10:35:41 +0200

    Added (tests for) method, view and route for posts#show

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :post do
     title { Faker::Book.title }
     content { Faker::Hipster.paragraph }
+    created_at { Faker::Date.backward }
   end
 end
--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :routing do
-    describe "GET /" do
+    describe "Routing" do
       it "routes to posts#index" do
         expect(get: "/").to route_to("posts#index")
+      end
+
+      it "routes to posts#show" do
+        expect(get: "/posts/1").to route_to("posts#show", id: "1")
       end
     end
 end
