338470ab9cfed58b3a2e564e975f3e7b7488aad0
noraeb <n.blomaard@gmail.com>
2016-05-10 18:44:02 +0200

    Routing for create post

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe PostsController, type: :routing do
       it "routes to posts#show" do
         expect(get: "/posts/1").to route_to("posts#show", id: "1")
       end
+
+      it "routes to #create" do
+      expect(:post => "/posts").to route_to("posts#create")
+    end
     end
 end
