e64e4b75fcce2f478e933968278812a9a6ab4d1f
noraeb <n.blomaard@gmail.com>
2016-05-10 15:02:32 +0200

    Migrated admin references to posts and added validations to post and admin model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it { is_expected.to belong_to(:admin) }
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:content) }
 end
