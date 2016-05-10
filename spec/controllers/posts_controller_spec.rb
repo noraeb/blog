8e94378c930c49c61f60120fbf46da385644fd89
noraeb <n.blomaard@gmail.com>
2016-05-10 14:12:11 +0200

    Added create post to controller and form in index view

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
 
   describe "GET #index" do
-    let(:posts) { create_list(:post, 5) }
+    let(:posts) { create_list(:post, 1) }
 
     it "assigns all posts to @posts" do
       get :index
@@ -11,4 +11,53 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET #show" do
+    let(:post) { FactoryGirl.create(:post) }
+
+    it "assigns the requested post as @post" do
+      get :show, { id: post.to_param }
+      expect(assigns(:post)).to eq post
+    end
+  end
+
+  describe "POST #create" do
+    let(:valid_attributes) do
+    { title: "Title", content: "So many tests!!" }
+    end
+
+    let(:invalid_attributes) do
+    { title: "" }
+    end
+
+    context "with valid params" do
+        it "creates a new Post" do
+          expect {
+            post :create, {post: valid_attributes}
+          }.to change(Post, :count).by(1)
+        end
+
+        it "assigns a new created post as @post" do
+          post :create, {post: valid_attributes}
+          expect(assigns(:post)).to be_a(Post)
+          expect(assigns(:post)).to be_persisted
+        end
+
+        it "redirects to the created post" do
+          post :create, {post: valid_attributes}
+          expect(response).to redirect_to(root_url)
+        end
+      end
+
+      context "with invalid params" do
+        it "assigns a newly created but unsaved post as @post" do
+          post :create, {post: invalid_attributes}
+          expect(assigns(:post)).to be_a_new(Post)
+        end
+
+        it "re-renders the 'new' template" do
+          post :create, {post: invalid_attributes}
+          expect(response).to render_template("new")
+        end
+      end
+  end
 end
