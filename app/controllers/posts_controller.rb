1ea437e0b078d7829d0b4ed4ec5ac26f03f3d9e6
noraeb <n.blomaard@gmail.com>
2016-05-10 15:29:23 +0200

    Added admin role to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,27 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
   end
 
   def show
     @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.admin = current_user_admin
+
+    if @post.save
+      redirect_to root_url, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -11,4 +11,60 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET #show" do
+    let(:post) { FactoryGirl.create(:post) }
+
+    it "assigns the requested post as @post" do
+      get :show, { id: post.to_param }
+      expect(assigns(:post)).to eq post
+    end
+  end
+
+  let(:admin) { Admin.create(email: "poster@social.com", password: "12345678") }
+
+  let(:valid_attributes) do
+  { title: "Title", content: "So many tests!!", admin: admin }
+  end
+
+  let(:invalid_attributes) do
+  { title: "" }
+  end
+  
+  describe "POST #create" do
+
+    it "is not accessible when not logged in" do
+         post :create, {post: valid_attributes}
+         expect(response).to redirect_to new_user_session_path
+       end
+    context "with valid params" do
+        it "creates a new Post" do
+          expect {
+            post :create, {post: valid_attributes}
+          }.to change(Post, :count).by(1)
+        end
+
+        it "assigns a new created post as @post" do
+          post :create, {post: valid_attributes}
+          expect(assigns(:post)).to be_a(Post)
+          expect(assigns(:post)).to be_persisted
+        end
+
+        it "redirects to the created post" do
+          post :create, {post: valid_attributes}
+          expect(response).to redirect_to(root_url)
+        end
+      end
+
+      context "with invalid params" do
+        it "assigns a newly created but unsaved post as @post" do
+          post :create, {post: invalid_attributes}
+          expect(assigns(:post)).to be_a_new(Post)
+        end
+
+        it "re-renders the 'new' template" do
+          post :create, {post: invalid_attributes}
+          expect(response).to render_template("new")
+        end
+      end
+  end
 end
