a6889acedcea4774bc400103b2b490266f4929e5
noraeb <n.blomaard@gmail.com>
2016-05-11 16:39:12 +0200

    Added comments form in posts#show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,11 +7,11 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(params.require(:comment).permit!)
 
     if @comment.save
-      redirect_to root_url, notice: 'Post was successfully created.'
+      redirect_to post_path(@post), notice: 'Post was successfully created.'
     else
       render :new
     end
@@ -23,6 +23,10 @@ class CommentsController < ApplicationController
   private
   def set_post
    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+  params.require(:comment).permit(:text, :user_id, :post)
   end
 
 end
