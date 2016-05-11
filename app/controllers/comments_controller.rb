4e74650975ab546729adf95c351c3ea6b2856679
noraeb <n.blomaard@gmail.com>
2016-05-11 16:10:10 +0200

    Comment create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,28 @@
 class CommentsController < ApplicationController
-def index
-end
+  before_action :set_post
+  def index
+  end
 
-def show
-end
+  def show
+  end
 
-def create
-end
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
 
-def destroy
-end
+    if @comment.save
+      redirect_to root_url, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def set_post
+   @post = Post.find(params[:post_id])
+  end
+
 end
