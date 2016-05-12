6a1a9b0afea1cca708e389adef16676294105242
noraeb <n.blomaard@gmail.com>
2016-05-12 13:50:58 +0200

    Fixed tests for views

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "posts/new.html.erb", type: :view do
-  let(:post) { FactoryGirl.post(create) }
+  before do
+    assign(:post, Post.new)
+    render
+  end
+
   it "renders new post form" do
-    assert_select "form[action=?][method=?]", new_post_path, "post" do
+    assert_select "form[action=?][method=?]", posts_path, "post" do
       assert_select "textarea#post_title[name=?]", "post[title]"
       assert_select "textarea#post_content[name=?]", "post[content]"
       assert_select "input#[type=?][value=?]", "submit", "Create Post"
--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe "posts/show.html.erb", type: :view do
 let(:post) { FactoryGirl.create(:post)}
 
-before do
-  assign(:post, post)
-  render
-end
+  before do
+    assign(:post, post)
+    render
+  end
 
   it "renders the post title " do
     assert_select "h1", text: post.title, count: 1
   end
 
   it "renders the content of the post" do
-      assert_select "p", text: post.content, count: 1
+    assert_select "p", text: post.content, count: 1
   end
 
 
