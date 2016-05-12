84a65f078967489ffd18320b0be74383df8e8528
noraeb <n.blomaard@gmail.com>
2016-05-12 11:02:54 +0200

    ImageUploader for posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511151857) do
+ActiveRecord::Schema.define(version: 20160512083433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160511151857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "admin_id"
+    t.string   "image"
   end
 
   add_index "posts", ["admin_id"], name: "index_posts_on_admin_id", using: :btree
