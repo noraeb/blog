c01e27ffe9c877d249b8c396657e05f8d2867da6
noraeb <n.blomaard@gmail.com>
2016-05-20 11:55:45 +0200

    Added cloudinary

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,7 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  include Cloudinary::CarrierWave
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -25,7 +25,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-  # 
+  #
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
